379e58662af454bc7201272c5fa918af62ba842e
nakanoyuki <yukich41@gmail.com>
2023-03-31 23:58:31 +0900

    Chart.js導入

---
+++ src/components/ChartGraph.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const ChartGraph = () => {
+  const options = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: "収支グラフ",
+      },
+    },
+  };
+
+  const labels = [
+    "1月",
+    "2月",
+    "3月",
+    "4月",
+    "5月",
+    "6月",
+    "7月",
+    "8月",
+    "9月",
+    "10月",
+    "11月",
+    "12月",
+  ];
+
+  const add = 1000 + 2000;
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "データ1",
+        data: [0, add],
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <>
+      <p>支出</p>
+      <Line data={data} options={options} />
+    </>
+  );
+};
+
+export default ChartGraph;
